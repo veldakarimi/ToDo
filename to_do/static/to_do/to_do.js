7603bc95992c29728483f9e7d9117add3e909062
DenoGeek <kariukidennisde@gmail.com>
2020-04-18 23:43:49 +0300

    The fix of the fixes

--- to_do/static/to_do/to_do.js
+++ to_do/static/to_do/to_do.js
@@ -1,14 +1,19 @@
 const fn = () => {
             'use strict';
             angular.module('to_do.demo', [])
-                .controller('To_doController', ['$scope', '$http', To_doController]);
+            .config(function($httpProvider) {
+                $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+                $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+            })
+            .controller('To_doController', ['$scope', '$http', To_doController]);
+            
             function To_doController($scope, $http) {
                 $scope.add = function(list, title){
+
                     var card = {
                         list: list.id,
                         title: title
                     };
-
 
                     $http.post('/to_do/cards/', card).then(function(response)
                         {
@@ -16,16 +21,23 @@ const fn = () => {
                         },
                         function(){
                             alert('could not create card')
-
-
-                    });
+                            }
+                    );
 
                     
                 };
 
                 $scope.data = [];
+
                 $http.get('/to_do/lists/').then(function(response){
                     $scope.data = response.data;
                 });
-                }
-        fn();
+            }
+        }
+        //This bracket was not here
+
+fn();
+
+//pale juu kuna config ya csrf
+//read more here
+//https://django-angular.readthedocs.io/en/latest/csrf-protection.html
